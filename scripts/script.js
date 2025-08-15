c792ab5bc9087149fbb8aeba7460040b24ae1846
mhmdjawad1 <baghdadijawad74@gmail.com>
2025-08-15 06:24:28 -0700

    Add tasks

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+function renderTasks(filter = 'all') {
+  const taskList = document.getElementById('task-list');
+  taskList.innerHTML = '';
